c2143cdba62b9750549a80d9b8ff1a53bebe6d9e
Shota Sotiris Oniani <shota@shotaoniani.ge>
2025-11-23 22:52:30 +0100

    Tailored the text on the placeholder home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,21 +21,8 @@ export default function Home() {
             </p>
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+            The <b>Presence Web Portfolio</b>. <br></br> <br></br>
+            Now built from the ground up for <br></br> <i>efficiency</i> and <i>visual appeal</i>.
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
