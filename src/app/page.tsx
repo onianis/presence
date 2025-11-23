4c5205d0aafc35a00b8f36b5c712fcbd7c7861b2
Shota Sotiris Oniani <shota@shotaoniani.ge>
2025-11-23 22:56:17 +0100

    Changed buttons on main placeholder page.
    Buttons now reflect the temporary navigation layout.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,62 +22,41 @@ export default function Home() {
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             The <b>Presence Web Portfolio</b>. <br></br> <br></br>
-            Now built from the ground up for <br></br> <i>efficiency</i> and <i>visual appeal</i>.
+            Now built from the ground up for <br></br> <i>efficiency</i> and{" "}
+            <i>visual appeal</i>.
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            href="/resume"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
+            Resume
           </a>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
+            href="/projects"
             rel="noopener noreferrer"
           >
-            Documentation
+            Projects
+          </a>
+          <a
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            href="/hashes"
+            rel="noopener noreferrer"
+          >
+            Hashes
+          </a>
+          <a
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            href="/about"
+            rel="noopener noreferrer"
+          >
+            About
           </a>
         </div>
         <div className="mt-8">
-          <Link
-            href="/resume"
-            className="text-sm text-sky-400 hover:text-sky-300 underline"
-          >
-            Go to Resume
-          </Link>
-          <br></br>
-          <Link
-            href="/projects"
-            className="text-sm text-sky-400 hover:text-sky-300 underline"
-          >
-            Go to Projects
-          </Link>
-          <br></br>
-          <Link
-            href="/hashes"
-            className="text-sm text-sky-400 hover:text-sky-300 underline"
-          >
-            Go to Hashes
-          </Link>
-          <br></br>
-          <Link
-            href="/about"
-            className="text-sm text-sky-400 hover:text-sky-300 underline"
-          >
-            Go to About
-          </Link>
         </div>
       </main>
     </div>
