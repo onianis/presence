ceff931db39ce2ed50d48b7d5aefeaa4ecbce98c
Shota Sotiris Oniani <shota@shotaoniani.ge>
2025-11-24 19:33:41 +0100

    Integrated navbar.tsx into core layout.tsx file.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Navbar } from "@/components/navbar";
 
 export const metadata: Metadata = {
   title: "The Presence Web Portfolio",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">
+      <body className="antialiased bg-zinc-50 font-sans dark:bg-black">
+        { }
+        <Navbar />
+        { }
         {children}
       </body>
     </html>
