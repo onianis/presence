c133100b002c19d58300c44ac2a0366460b0ec04
Shota Sotiris Oniani <shota@shotaoniani.ge>
2025-11-24 20:15:45 +0100

    Removed Next.js image from home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,6 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-background font-sans">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-primary-40 sm:items-start">
-        <Image
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-display leading-10 tracking-tight text-accent-1">
             New website. New architecture.{" "}
