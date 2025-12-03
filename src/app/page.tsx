e6910e10abd9f6dc6996bb1691540c755ac21aef
Shota Sotiris Oniani <shota@shotaoniani.ge>
2025-12-03 22:56:28 +0100

    Reworked temporary homepage layout.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-background font-sans">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-primary-40 sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-display leading-10 tracking-tight text-accent-1">
-            New website. New architecture.{" "}
-            <p>
-              <b>Better results.</b>
-            </p>
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-accent-20">
-            The <b>Presence Web Portfolio</b>. <br /> <br />
-            Now built from the ground up for <br /> <i>efficiency</i> and{" "}
-            <i>visual appeal</i>.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+    <div className="flex min-h-screen items-center justify-center">
+      <main className="flex max-w-7xl w-full min-h-screen items-center justify-center">
+        <div className="flex flex-row flex-nowrap w-full justify-around">
           <Link
             className="flex h-12 w-full items-center justify-center rounded-full border border-border bg-primary-20 font-mono text-accent-1 px-5 transition-colors hover:bg-primary-10 md:w-[158px]"
             href="/resume"
@@ -49,8 +36,8 @@ export default function Home() {
             About
           </Link>
         </div>
-        <div className="mt-8" />
       </main>
+        <div className="mt-8" />
     </div>
   );
 }
