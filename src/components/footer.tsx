29048ac017c93c1d03105e868caaa499807cef3a
Shota Sotiris Oniani <shota@shotaoniani.ge>
2025-12-03 22:37:37 +0100

    Major footer update.
    - Added /legal link to footer.
    
    - (hopefully) finalized layout on both mobile and desktop.
    
    - Added /about link to footer, which is technically redundant, but having just one auxilliary link the footer just looks bad.
    
    - Made all footer text unselectable.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,12 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import Link from "next/link";
 import Image from "next/image";
 
 export function Footer() {
   return (
     <footer className="bg-primary-50 text-accent-10">
-      <div className="w-full flex flex-col flex-wrap gap-0 md:gap-y-2 py-3 md:py-5">
-        <div className="mx-auto flex max-w-7xl items-start justify-between gap-4 px-4 pb-0 sm:px-6 text-sm w-full">
+      <div className="w-full flex flex-col flex-nowrap gap-y-4 md:gap-y-2 py-3 md:pt-5 md:pb-3 select-none">
+        <div className="mx-auto flex flex-row max-w-7xl items-start justify-between gap-4 px-4 text-sm w-full">
           <div className="flex items-center">
             <Image
               src="/icons/logo_color_512.png"
@@ -15,7 +16,6 @@ export function Footer() {
               className="w-16 h-16 md:w-20 md:h-20"
             />
             <div className="font-display font-bold text-2xl md:text-3xl grid grid-cols-1 grid-rows-3 [&>span]:leading-[0.75] [&>span]:tracking-tighter ml-1">
-              {/* [&>span]:border [&>span]:border-border */}
               <span className="row-start-1">shota</span>
               <span className="row-start-2">sotiris</span>
               <span className="row-start-0">oniani.</span>
@@ -23,7 +23,7 @@ export function Footer() {
           </div>
 
           {/* Right: placeholder contact buttons */}
-          <div className="grid grid-cols-2 md:grid-cols-4 items-center gap-1 md:gap-2 *:w-8 *:h-8 *:md:w-12 *:md:h-12 *:md:ml-2">
+          <div className="grid grid-cols-2 md:grid-cols-4 items-center gap-1 md:gap-4 *:w-8 *:h-8 *:md:w-12 *:md:h-12">
             <Link
               href="mailto:fake@email.com"
               className="group relative inline-block"
@@ -99,9 +99,26 @@ export function Footer() {
             </Link>
           </div>
         </div>
-        <div className="mx-auto max-w-7xl w-full flex flex-col px-4 justify-center [&>span]:tracking-tighter">
-          <span className="font-display font-light text-md md:text-3xl text-accent-30">The Presence Web Portfolio <span className="font-mono text-accent-20 text-md align-middle">v1.0</span></span>
-          <span className="font-display text-sm md:text-lg">all rights reserved ©2025</span>
+        <div className="flex flex-row flex-nowrap mx-auto max-w-7xl w-full justify-between px-4">
+          <div className="flex flex-col justify-center [&>span]:tracking-tighter">
+            <span className="font-display font-light text-md md:text-3xl text-accent-30">
+              the Presence Web Portfolio{" "}
+              <span className="font-mono text-accent-20 text-sm md:text-[1.3rem] align-middle">
+                // v1.0
+              </span>
+            </span>
+            <span className="font-display text-sm md:text-lg">
+              all rights reserved ©2025
+            </span>
+          </div>
+          <div className="flex flex-col justify-end text-accent-30 *:text-right *:hover:text-accent-10 *:transition-all *:duration-300 *:linear text-sm md:text-lg">
+            <Link href="/legal" className="font-mono">
+              legal
+            </Link>
+            <Link href="/about" className="font-mono">
+              about
+            </Link>
+          </div>
         </div>
       </div>
     </footer>
